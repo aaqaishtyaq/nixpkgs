3008d57be206163b834b0a3c5fd000c5b1927ce9
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2024-12-20 22:06:38 +0530

    fix(tmux): remove zombie session 0
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -58,7 +58,6 @@ in {
         LC_ALL = "en_US.UTF-8";
         LC_CTYPE = "en_US";
         LC_MESSAGES="en_US";
-        # TERM="xterm-256color";
       };
       initExtra = ''
         if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then

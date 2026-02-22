6e4c48ca99578c71cc9df9416b5be7137d02b718
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2026-02-22 20:18:06 +0530

    add beads, ollama and zed
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -28,7 +28,9 @@ in
     "aaqaishtyaq/tap"
   ];
 
-  # homebrew.brews = [];
+  homebrew.brews = [
+    "beads"
+  ];
 
   # If an app isn't available in the Mac App Store, or the version in the App Store has
   # limitiations, e.g., Transmit, install the Homebrew Cask.
@@ -53,6 +55,8 @@ in
     "vlc"
     "iina"
     "transmission"
+    "ollama"
+    "zed"
   ];
 
   home-manager.sharedModules = [

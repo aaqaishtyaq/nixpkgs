2f96b87910eb5dcc285063ca799954349fe32fad
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2026-03-25 00:57:52 +0530

    update flake, add overlay for direnv cgo build error
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

--- flake.nix
+++ flake.nix
@@ -139,6 +139,16 @@
               }
             );
           };
+
+        direnv-cgo-fix =
+          _: prev:
+          optionalAttrs prev.stdenv.isDarwin {
+            direnv = prev.direnv.overrideAttrs (old: {
+              env = (old.env or { }) // {
+                CGO_ENABLED = "1";
+              };
+            });
+          };
       };
 
       darwinModules = {
